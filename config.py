48276eefece224890df041118e2a72727495b0d4
KimLien4473 <lienntk4473@ut.edu.vn>
2026-06-15 13:49:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+class Config:
+    # Chuỗi bí mật để bảo mật session (đăng nhập)
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ma-bao-mat-chu-tich-nhom-sms'
+    
+    # Cấu hình kết nối MySQL (Thay đổi user, password, db_name theo thực tế của nhóm)
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'your_password'
+    MYSQL_DB = 'sms_database'
