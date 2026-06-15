62f7118774fa27bf773b539fee8deccb8a476b5d
KimLien4473 <lienntk4473@ut.edu.vn>
2026-06-15 14:59:51 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
+import mysql.connector
+from config import Config
 
+
+def get_db_connection():
+
+    try:
+
+        connection = mysql.connector.connect(
+            host=Config.MYSQL_HOST,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD,
+            database=Config.MYSQL_DB
+        )
+
+        return connection
+
+    except mysql.connector.Error as err:
+
+        print("Lỗi kết nối MySQL:", err)
+
+        return None
